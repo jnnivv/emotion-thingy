76830581c358bd1aa318afe68b20fa62723bab35
William Young <mr.william.a.young@gmail.com>
2017-11-04 19:05:25 -0700

    img_encoded not going through

--- renderer.js
+++ renderer.js
@@ -4,7 +4,6 @@
 
 const fs = require("fs")
 const NodeWebcam = require( "node-webcam" )
-const queryCognitive = require("./queryCognitive.js")
 
 makeblob = function (dataURL) {
     var BASE64_MARKER = ';base64,';
@@ -26,6 +25,32 @@ makeblob = function (dataURL) {
     }
 
     return new Blob([uInt8Array], { type: contentType });
+}
+
+function sendToCognitive(img_encoded) {
+    $.ajax({
+        // NOTE: You must use the same location in your REST call as you used to obtain your subscription keys.
+        //   For example, if you obtained your subscription keys from westcentralus, replace "westus" in the
+        //   URL below with "westcentralus".
+        url: "https://westus.api.cognitive.microsoft.com/emotion/v1.0/recognize",
+        beforeSend: function(xhrObj){
+            // Request headers
+            xhrObj.setRequestHeader("Content-Type","application/octet-stream")
+
+            // NOTE: Replace the "Ocp-Apim-Subscription-Key" value with a valid subscription key.
+            xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key","a59001fc2df14b67ab2364ef8c324318")
+        },
+        type: "POST",
+        // Request body
+        data: makeblob(img_encoded)
+        //data: '{"url": "https://www.maybelline.com/~/media/mny/us/face-makeup/modules/masthead/maybelline-fit-me-foundation-powder-face-herieth-paul-1x1.jpg?h=320&w=320&la=en-US&hash=3B5E9C176BE1DD97CB6BC8F5CD2F5C7BBA440695"}',
+    })
+    .done(function(data) {
+        $('#result').text(JSON.stringify(data))
+    })
+    .fail(function(err) {
+        $('#result').text(JSON.stringify(err))
+    })
 }
 
 const opts = {
@@ -69,7 +94,7 @@ function capture() {
         const img_encoded = `data:image/jpeg;base64,${enc}`
         $("#message").text(img_encoded)
         $("#image-stage").attr("src", img_encoded)
-        queryCognitive(img_encoded)
+        sendToCognitive(img_encoded)
     })
 }
 capture()
