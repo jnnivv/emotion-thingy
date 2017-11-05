9c3fc1d10118e44e658c2b9f4f80a37b3f191310
jnnivv <jnnivv@gmail.com>
2017-11-05 18:50:02 -0500

    for da andrew to da look at

--- app/js/renderer.js
+++ app/js/renderer.js
@@ -4,10 +4,13 @@ const spotify = require("./spotify")
 
 document.querySelector("#capture-button").addEventListener("click", webcam.capture)
 
-fs.readFile("./app/.key", function(err, data) {
-  if (err) {
-    alert(err)
-  } else {
-    spotify.AuthRequest(data)
-  }
+
+spotify.AuthRequest(spotify.getRecommendations, {
+  limit: 5,
+  max_acousticness: 0.5,
+  max_danceability: 0.5,
+  max_energy: 0.5,
+  max_instrumentalness: 0.5,
+  max_tempo: 0.5,
+  max_valence: 0.5
 })
